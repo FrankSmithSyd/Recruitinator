6cbc8c60760dd8ae3d7e1fcfcf1a707decbab939
Frank Smith <30485093+FrankSmithSyd@users.noreply.github.com>
2021-02-17 11:11:31 +1100

    REC-5: Added some more entities

---
+++ Core/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Entities
+{
+    public class Person
+    {
+        private int Id { get; }
+        private string Name { get; }
+        private IEnumerable<SkillTag> SkillTags { get; }
+
+        protected Person(int id, string name, string skillTags)
+        {
+            Id = id;
+            Name = name;
+            SkillTags = skillTags.Split(',').Select(x => new SkillTag(x));    //TODO: Maybe only get distinct skills, I'm noticing some duplicates in the dataset.
+
+        }
+    }
+}
---
+++ Core/Entities/SkillTag.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class SkillTag
+    {
+        public string Name { get; set; }
+
+        public SkillTag(string tagName)
+        {
+            Name = tagName;
+        }
+    }
+}
