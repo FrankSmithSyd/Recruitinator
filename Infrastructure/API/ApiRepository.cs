ce91616ecd9610cb764799aaee09d5afc02a8f28
Frank Smith <30485093+FrankSmithSyd@users.noreply.github.com>
2021-02-18 09:50:16 +1100

    REC-5: Added GetJobs, and refactored ApiRepository.cs a tad to remove duplicate code.

--- Infrastructure/API/ApiRepository.cs
+++ Infrastructure/API/ApiRepository.cs
@@ -21,16 +21,11 @@ namespace Infrastructure
         {
             _api = api ?? new JobAdder();    // TODO FRANK: remove this null coalescer, we ALWAYS want to inject it in Startup.cs. Also don't want to directly specifiy JobAdder API here.
             _restClient = new RestClient(_api.clientUri);
-            // _clientApi = restClient ?? new RestClient("http://private-76432-jobadder1.apiary-mock.com/");
         }
         
         public IEnumerable<Candidate> GetCandidates()
         {
-            var request = _api.GetCandidatesAction;
-            var response = _restClient.Execute(request);
-            var jsonCandidates = JArray.Parse(response.Content).ToString();
-            
-            // Map the Jarray objects to candidates.
+            var jsonCandidates = ExecuteExternalApiRequest(_api.GetCandidatesAction);
             var candidates = JsonConvert.DeserializeObject<IEnumerable<Candidate>>(jsonCandidates);
             
             return candidates;
@@ -38,7 +33,17 @@ namespace Infrastructure
 
         public IEnumerable<Job> GetJobs()
         {
-            throw new NotImplementedException();
+            var jsonJobs = ExecuteExternalApiRequest(_api.GetJobsAction);
+            var jobs = JsonConvert.DeserializeObject<IEnumerable<Job>>(jsonJobs);
+            
+            return jobs;
+        }
+
+        private string ExecuteExternalApiRequest(RestRequest request)
+        {
+            var response = _restClient.Execute(request);
+            var jsonCandidates = JArray.Parse(response.Content).ToString();
+            return jsonCandidates;
         }
     }
 }
