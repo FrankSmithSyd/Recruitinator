c4b473da03cc41afa1f2afaf16ec15bc8ae4f162
Frank Smith <30485093+FrankSmithSyd@users.noreply.github.com>
2021-02-19 08:59:44 +1100

    REC-4: Fixed a bug where due to the addition of a 2nd constructor to the Person class, Newtonsoft.JsonDeserialize was throwing an error. This was fixed by specifically tagging the needed constructor with the [JsonConstructor]. Also removed the sipfilrous base class implementation "Person" from Candidate, this was needlessly complicating the implementation.

--- Core/Entities/Candidate.cs
+++ Core/Entities/Candidate.cs
@@ -1,10 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Core.Entities
 {
-    public class Candidate : Person
+    public class Candidate
     {
-        public Candidate(int candidateId, string name, string skillTags) : base(candidateId, name, skillTags) {}
-        public Candidate(int candidateId, string name, IEnumerable<SkillTag> skillTags) : base(candidateId, name, skillTags) {}
+        public int CandidateId { get; }
+        public string Name { get; }
+        public IEnumerable<SkillTag> SkillTags { get; }
+        
+        [JsonConstructor]
+        public Candidate(int candidateId, string name, string skillTags)
+        {
+            CandidateId = candidateId;
+            Name = name;
+            SkillTags = skillTags.Split(',').Select(x => new SkillTag(x));
+        }
+        
+        public Candidate(int candidateId, string name, IEnumerable<SkillTag> skillTags)
+        {
+            CandidateId = candidateId;
+            Name = name;
+            SkillTags = skillTags;
+        }
+        
+        public string GetAllSkillsAsCommaSeparatedString()
+        {
+            var skillTagNames = SkillTags.Select(x => x.Name);
+            return string.Join(",", skillTagNames);
+        }
     }
 }
