5e5e0944b0130c00c2a71ae2539216a131a8a750
Frank Smith <30485093+FrankSmithSyd@users.noreply.github.com>
2021-02-18 12:19:39 +1100

    REC-2: Added candidates controller and basic views for frontend.

---
+++ Web/Controllers/CandidatesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Services;
+
+namespace Web.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        private readonly ICandidateService _service;
+        
+        public CandidatesController(ICandidateService service)
+        {
+            _service = service;            
+        }
+        
+        public IActionResult Index()
+        {
+            var candidates = _service.GetAllCandidates();
+            
+            return View(candidates);
+        }
+    }
+}
